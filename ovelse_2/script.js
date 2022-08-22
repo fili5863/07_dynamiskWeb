580af9e95f39a89a4fff2fb4da4121e373541d4b
fili5863​ <fili5863@stud.kea.dk>
2022-08-22 13:33:23 +0200

    ovelse 2 final

---
+++ ovelse_2/script.js
@@ -0,0 +1,11 @@
+const time = new Date().getHours();
+
+if (time > 5 && time < 10) {
+  console.log("Godmorgen");
+} else if (time > 10 && time < 18) {
+  console.log("Goddag");
+} else if (time > 18 && time < 24) {
+  console.log("Godaften");
+} else {
+  console.log("Godnat");
+}
