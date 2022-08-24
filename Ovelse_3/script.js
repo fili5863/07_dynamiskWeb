4d23629bec1359b5bf8641d0f4af87796a2fd27b
Jachob Westh <jachobwesth@gmail.com>
2022-08-24 09:55:57 +0200

    ovelser overflytning

--- Ovelse_3/script.js
+++ Ovelse_3/script.js
@@ -1,20 +1,20 @@
 document.querySelector("button").addEventListener("click", tjekTal);
-let forsog = 0;
+let forsoeg = 0;
 const ranNum = Math.floor(Math.random() * 11);
 console.log(ranNum);
 const tal = document.querySelector("tal");
 
 function tjekTal() {
-  forsog++;
+  forsoeg++;
   const brugerTal = document.querySelector("#tal").value;
   if (ranNum > brugerTal) {
-    alert("Øv!" + brugerTal + " var for lavt. Prøv igen :)");
+    alert("Øv! " + brugerTal + " var for lavt. Prøv igen :)");
     console.log(ranNum, brugerTal);
   } else if (ranNum < brugerTal) {
-    alert("Øv!" + brugerTal + " var for højt. Prøv igen :)");
+    alert("Øv! " + brugerTal + " var for højt. Prøv igen :)");
     console.log(ranNum, brugerTal);
   } else {
-    alert(brugerTal + " er rigtigt!" + " Du brugte " + forsog + " forsøg. Prøv igen");
+    alert(brugerTal + " er rigtigt!" + " Du brugte " + forsoeg + " forsøg. Prøv igen");
     console.log("Du gættede rigtigt!");
     location.reload();
   }
