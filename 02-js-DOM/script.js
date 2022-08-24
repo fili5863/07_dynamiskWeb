11b2a63e813c24f417b655cec8680aebf20590b7
fili5863​ <fili5863@stud.kea.dk>
2022-08-24 11:33:36 +0200

    nyt

---
+++ 02-js-DOM/script.js
@@ -0,0 +1,23 @@
+const overskrift = document.querySelector("h1");
+const underOverskrift = document.querySelector("h2");
+const article1 = document.querySelector("article:nth-child(1)");
+const img1 = document.querySelector("article:nth-child(1) img");
+const article1Heading = document.querySelector("article:nth-child(1) h3");
+const article1Content = document.querySelector("article:nth-child(1) p");
+const article2 = document.querySelector("article:nth-child(2)");
+const img2 = document.querySelector("article:nth-child(2) img");
+const article2Heading = document.querySelector("article:nth-child(2) h3");
+const article2Content = document.querySelector("article:nth-child(2) p");
+
+console.log(
+  overskrift,
+  underOverskrift,
+  article1,
+  img1,
+  article1Heading,
+  article1Content,
+  article2,
+  img2,
+  article2Heading,
+  article2Content
+);
